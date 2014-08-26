efa35383fff0cea15a6ab469714c6730c0cb099b
lywaterman <liuyang124846@gmail.com>
2014-08-26 17:11:06 +0800

    Update reloader.erl

--- src/reloader.erl
+++ src/reloader.erl
@@ -123,10 +123,34 @@ doit(From, To) ->
                        [Filename, Reason]),
              error
      end || {Module, Filename} <- code:all_loaded(), is_list(Filename)].
+     
+%%reload(Module) ->
+%%    io:format("Reloading ~p ...", [Module]),
+%%    code:purge(Module),
+%%    case code:load_file(Module) of
+%%        {module, Module} ->
+%%            io:format(" ok.~n"),
+%%            case erlang:function_exported(Module, test, 0) of
+%%                true ->
+%%                    io:format(" - Calling ~p:test() ...", [Module]),
+%%                    case catch Module:test() of
+%%                        ok ->
+%%                            io:format(" ok.~n"),
+%%                            reload;
+%%                        Reason ->
+%%                            io:format(" fail: ~p.~n", [Reason]),
+%%                            reload_but_test_failed
+%%                    end;
+%%                false ->
+%%                    reload
+%%            end;
+%%        {error, Reason} ->
+%%            io:format(" fail: ~p.~n", [Reason]),
+%%            error
+%%    end.
 
 reload(Module) ->
     io:format("Reloading ~p ...", [Module]),
-    %%code:purge(Module),
     case code:soft_purge(Module) andalso code:load_file(Module) of
         false ->
             io:format("~p is using", [Module]);
